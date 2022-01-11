926c44280a4ab08426448b1e57c1c0d4d0ae98a2
Claire List <claire.list2501@gmail.com>
2022-01-11 09:18:44 -0700

    frame built

--- api/server.js
+++ api/server.js
@@ -1,3 +1,25 @@
 // BUILD YOUR SERVER HERE
+//PSEUD 'DATA SECTION'
+const express = require('express');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// INSTANCE OF EXPRESS APP
+const server=express(); //invoked
+
+// GLOBAL MIDDLEWARE
+server.use(express.json()); //express is a framework of sorts that will build a server
+
+//PSEUD 'LOGIC SECTIONR'
+
+//NOTE all USERS have an id, a name, and a bio -- all of which are required
+//// GET endpoint : /api/users --  get all users
+
+//// GET by id endpoint : /api/users/:id -- get single user by id
+
+//// POST endpoint : /api/users -- create a user (use DATA from request body !)
+
+//// PUT endpoint : /api/users/:id -- update a user by id
+
+//// DELETE endpoint : /api/users/:id -- deletes user by id
+
+
+module.exports = server;
